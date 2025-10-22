98876424199b1a5a821f1a88f8593d8e3db908cb
telluricaquarian <llewellyn@telluricaquarian.com>
2025-10-22 22:56:11 +1100

    Fix logo component typing for Next build

--- public/SolarLogo.tsx
+++ public/SolarLogo.tsx
@@ -1,5 +1,13 @@
+// public/SolarLogo.tsx
 import Image from "next/image";
 
-export const SolarLogo = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
-  <Image src="/images/aclr77.svg" alt="Areculateir Logo" width={140} height={48} priority {...props} />
+export const SolarLogo = ({ className }: { className?: string }) => (
+  <Image
+    src="/images/aclr77.svg"
+    alt="Areculateir Logo"
+    width={140}
+    height={48}
+    priority
+    className={className}
+  />
 );
