e2e8e62318bde102428f3c318f07172a9dec4505
telluricaquarian <llewellyn@telluricaquarian.com>
2025-11-15 17:55:55 +1100

    Fix missing opening tag in Hero.tsx

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,9 +1,9 @@
+import QuoteModal from "@/components/ui/WaitlistModal";
 import { RiArrowRightUpLine } from "@remixicon/react";
+import { useState } from "react";
 import { FadeContainer, FadeDiv, FadeSpan } from "../Fade";
 import { HeroVideoDialog } from "./hero-video-dialog";
 import GameOfLife from "./HeroBackground";
-import { useState } from "react";
-import QuoteModal from "@/components/ui/WaitlistModal";
 
 export function Hero() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,13 +12,7 @@ export function Hero() {
     <section aria-label="hero">
       <FadeContainer className="relative flex flex-col items-center justify-center">
         <FadeDiv className="mx-auto">
-          
-            aria-label="View latest update the changelog page"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-auto w-full"
-          >
+          <a aria-label="View latest update the changelog page" href="#" target="_blank" rel="noopener noreferrer" className="mx-auto w-full">
             <div className="inline-flex max-w-full items-center gap-3 rounded-full bg-white/5 px-2.5 py-0.5 pr-3 pl-0.5 font-medium text-gray-900 ring-1 shadow-lg shadow-orange-400/20 ring-black/10 filter backdrop-blur-[1px] transition-colors hover:bg-orange-500/2.5 focus:outline-hidden sm:text-sm">
               <span className="shrink-0 truncate rounded-full border bg-gray-50 px-2.5 py-1 text-sm text-gray-600 sm:text-xs">
                 Grow
@@ -69,11 +63,11 @@ export function Hero() {
         </div>
       </FadeContainer>
 
-      <QuoteModal 
-        isOpen={isModalOpen} 
+      <QuoteModal
+        isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         modalType="prototype"
       />
-    </section >
+    </section>
   );
 }
