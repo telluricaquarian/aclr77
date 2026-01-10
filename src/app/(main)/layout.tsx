5e7175d0e62564c74a7622c971deb6eadb1cb4f3
Llewellyn Y. Fisher <llewellyny.fisher@adsl-172-14-80-169.dsl.frs2ca.sbcglobal.net>
2026-01-09 19:59:49 -0800

    Hide main navbar on mobile

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -15,7 +15,11 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
 
                 {/* Main column */}
                 <div className="min-w-0 flex-1 lg:pl-64">
-                    <NavBar />
+                    {/* NavBar (desktop only) */}
+                    <div className="hidden lg:block">
+                        <NavBar />
+                    </div>
+
                     {children}
                     <Footer />
                 </div>
